5f1b2566b18174f8c34cbfeba0ac1db0aaae85fb
Adami-Ale06 <alarico06.aa@gmail.com>
2025-11-03 12:51:46 +0100

    variante

---
+++ es1.py
@@ -0,0 +1,9 @@
+num1 = float(input("inserisci il primo numero: "))
+num2 = float(input("inserisci il secondo numero: ")) #inserimento secondo numero
+if num2 == 0:
+    print("Errore nella divisione")
+else:
+    rapporto = num1 / num2
+print(f"il rapporto tra {num1} e {num2} e': {rapporto}")
+        
+        
